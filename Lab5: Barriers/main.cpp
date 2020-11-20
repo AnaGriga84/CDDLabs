76a880145b49cc5833ad5befa42143cd9d8aac93
Ana Griga <AnaGrigaMail@gmail.com>
2020-11-20 13:39:52 +0000

    called phaseOne and phaseTwo in main

--- Lab5: Barriers/main.cpp
+++ Lab5: Barriers/main.cpp
@@ -21,12 +21,12 @@ void barrierTask(std::shared_ptr<Barrier> theBarrier, int numLoops)
 	for (int i = 0; i < numLoops; ++i)
 	{
 		//Do first bit of task
-		std::cout << "A";
+		std::cout << "A"<<i;
 		//Barrier
-		theBarrier->wait();
+		theBarrier->phaseOne();
 		//Do second half of task
-		std::cout << "B";
-		theBarrier->wait();
+		std::cout << "B"<<i;
+		theBarrier->phaseTwo();
 	}
 }
 
